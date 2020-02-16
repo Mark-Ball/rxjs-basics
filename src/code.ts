a5460b4c19a74e65f1ad554fec4cac8a9ce84645
Mark-Ball <markball111@gmail.com>
2020-02-16 17:21:28 +1100

    up to part on subjects

--- src/code.ts
+++ src/code.ts
@@ -1,12 +1,26 @@
-import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
-// observable create accepts 1 argument, which is the subscribe function
-// subscribe function accepts observer argument
-const observable = Observable.create((observer: any) => {
-    observer.next('Hello there')
-});
+const subject = new BehaviorSubject('First');
 
-observable.subscribe((x: any) => addItem(x));
+subject.subscribe(
+    data => addItem('Observer 1: ' + data),
+    err => addItem(err),
+    () => addItem('Observer 1 Completed')
+);
+
+subject.next('The first thing has been sent');
+subject.next('...Observer 2 is about to subscribe');
+
+const observer2 = subject.subscribe(
+    data => addItem('Observer 2: ' + data)
+);
+
+subject.next('The second thing has been sent');
+subject.next('The third thing has been sent');
+
+observer2.unsubscribe();
+
+subject.next('The final thing has been sent');
 
 function addItem(val:any) {
     var node = document.createElement("li");
